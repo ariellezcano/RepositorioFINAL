abcba43e0cfc759769dec540c9a1bcfde0c29646
ariellezcano <ariellezcano1995@gmail.com>
2021-08-12 13:26:49 -0300

    Siguen las modificaciones de configuración

--- proyecto/proyecto/settings/base.py
+++ proyecto/proyecto/settings/base.py
@@ -72,18 +72,7 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
 
-        'ENGINE': 'Sql_server.pyodbc',
-        'NAME': 'BDINFO',
-        'Trusted_Connection':'yes',
-        'Host':'localhost\\SQLEXPRESS',
-        'Option':{
-            'driver':'SQL Server Native Client 11.0',
-        }
-    }
-}
 
 
 # Password validation
@@ -108,9 +97,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
@@ -123,3 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=(
+    os.path.join(os.path.dirname(BASE_DIR), 'static'),
+)
